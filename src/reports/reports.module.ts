b0e9cfc5769c5d1c46ac5fa2617b74a1a21af66a
Fran Barrena <francisco@kyso.io>
2023-03-06 18:00:32 +0100

    feat: removed everything for builder

--- src/reports/reports.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common'
-import { DatabaseModule } from '../database/database.module'
-import { ReportsController } from './reports.controller'
-
-@Module({
-    imports: [DatabaseModule],
-    controllers: [ReportsController],
-})
-export class ReportsModule {}
